32279e419367cf22e8a65c3e2a789b9502d0ae19
AoWangPhilly <aowangphilly@gmail.com>
2021-08-03 19:36:28 -0400

    Getting historical data with yahoo-finance

---
+++ src/data/stock_prices.js
@@ -0,0 +1,24 @@
+const yahooFinance = require("yahoo-finance");
+const fs = require("fs");
+const tickers = require("./tickers.json");
+
+// Should be saved into Firebase at some point, this is an example
+// of saving all the tickers in the S&P 500 to JSON files
+for (let ticker of tickers) {
+  yahooFinance.historical(
+    {
+      symbol: ticker,
+      // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+    },
+    function (err, quotes) {
+      fs.writeFile(
+        `${ticker}_stonks.json`,
+        JSON.stringify(quotes),
+        "utf8",
+        (err) => {
+          err ? console.log("error") : console.log("done");
+        }
+      );
+    }
+  );
+}
