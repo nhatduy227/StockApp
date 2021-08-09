06b4d8ea57bf84e48f89870eb97f6dc500142078
Ao Wang <aw3338@drexel.edu>
2021-08-09 19:47:46 -0400

    Revert "Landing pages"

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React, { useState, useEffect } from "react";
-import StockPage from "./components/StockPage/StockPage";
-import LandingPage from "./components/LandingPage/LandingPage";
-import Navbar from "./components/NavBar/NavBar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import useFetch from "react-fetch-hook";
+import Stock from "./components/Stock/Stock";
+import SearchBar from "./components/SearchBar/SearchBar";
+import DropDown from "./components/DropDown/DropDown";
 import "./App.css";
+import db from "./firebase";
+// import firebase from "firebase";
 
 function App() {
+  const [stocks, setStocks] = useState([]); 
+  useEffect(() => {
+    db.collection("stocks").onSnapshot((snapshot) => {
+      setStocks(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          image: doc.data().image,
+          score: doc.data().score,
+          title: doc.data().title,
+        }))
+      );
+    });
+  }, []);
+
   return (
-    <>
-      <Router>
-        <div className="grid-container">
-          <header>
-            {" "}
-            <Navbar />
-          </header>
-          <main>
-            <div className="content">
-              <div className="main">
-                <Switch>
-                  <Route path="/" exact component={LandingPage} />
-                  <Route path="/stockpage" exact component={StockPage} />
-                </Switch>
-              </div>
-            </div>
-          </main>
+    <div className="grid-container">
+      <header>
+        <a href="/">StockApp</a>
+      </header>
+      <main>
+        <div className="content">
+          <div className="main">
+            <SearchBar />
+            <DropDown />
+            <Stock stocks={stocks} />
+          </div>
         </div>
-      </Router>
-    </>
+      </main>
+      <footer>All right is reserved.</footer>
+    </div>
   );
 }
 
