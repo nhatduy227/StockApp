c95aa39070414c87fb57a7f7d2cffe947e6f1891
AoWangPhilly <aowangphilly@gmail.com>
2021-08-09 23:23:03 -0400

    Add gradient to graph to main color theme

--- src/components/Graph/Graphs.js
+++ src/components/Graph/Graphs.js
@@ -11,26 +11,38 @@ import React, { Component } from "react";
 
 let data = require("../../data/AAPL_stonks.json");
 
-function clean(data) {
+/**
+ * clean formats the date string into the JS datetime object
+ * and sorts the array of stock data by the date from earliest-latest
+ * @returns stock data
+ */
+function clean() {
   data.map((day) => (day.date = new Date(day.date)));
   data.sort((day1, day2) => {
-    // Compare the 2 dates
+    
     if (day1.date < day2.date) return -1;
     if (day1.date > day2.date) return 1;
     return 0;
   });
 
-  console.log(data);
   return data;
 }
 
 function Graph() {
   return (
     <ResponsiveContainer width="100%" height={400}>
-      <AreaChart data={clean(data)}>
-        <Area dataKey="close" />
+      <AreaChart data={clean()}>
+      <defs>
+          <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="0%" stopColor="#85F485" stopOpacity={0.4}/>
+            <stop offset="75%" stopColor="#85F485" stopOpacity={0.25}/>
+          </linearGradient>
+      </defs>
+        <Area dataKey="close" stroke='#85F485' fill="url(#color)"/>
         <XAxis dataKey="date" />
         <YAxis dataKey="close" />
+        <Tooltip/>
+        <CartesianGrid/>
       </AreaChart>
     </ResponsiveContainer>
   );
