5833cd66c3fdbe5724e70a7f9c2f591cff5e9d71
Nom Phan <np656@drexel.edu>
2021-08-11 23:02:10 -0400

    add filter by sector function

--- src/components/StockPage/StockPage.js
+++ src/components/StockPage/StockPage.js
@@ -7,6 +7,7 @@ import db from "../../firebase";
 export default function StockPage() {
   const [stocks, setStocks] = useState([]);
   const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("all");
   useEffect(() => {
     db.collection("stocks").onSnapshot((snapshot) => {
       setStocks(
@@ -15,6 +16,7 @@ export default function StockPage() {
           image: doc.data().image,
           score: doc.data().score,
           title: doc.data().title,
+          sector: doc.data().sector,
         }))
       );
     });
@@ -27,7 +29,9 @@ export default function StockPage() {
     const filtered_by_search = array.filter((s) =>
       s.title.toLowerCase().includes(search.toLowerCase())
     );
-    return filtered_by_search;
+    return filter === "all"
+      ? filtered_by_search
+      : filtered_by_search.filter((s) => s.sector?.toLowerCase() === filter);
   }
   function Stock({ stocks }) {
     return (
@@ -38,7 +42,9 @@ export default function StockPage() {
               <li key={stock.id}>
                 <div className="stock">
                   <img src={stock.image} alt={stock.title}></img>
-                  <p>{stock.title}</p>
+                  <p>
+                    {stock.title} - {stock.sector}
+                  </p>
                   <div className="stock-price">
                     <div>{stock.score}</div>
                     <button className="button primary">
@@ -53,9 +59,30 @@ export default function StockPage() {
       </div>
     );
   }
+  function Filter() {
+    return (
+      <section id="Filters">
+        <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+          <option value="all">All</option>
+          <option value="energy">Energy</option>
+          <option value="financials">Financials</option>
+          <option value="industrials">Industrials</option>
+          <option value="real estate">Real Estate</option>
+          <option value="consumer staples">Consumer Staples</option>
+          <option value="consumer discretionary">Consumer Discretionary</option>
+          <option value="information technology">Information Technology</option>
+          <option value="health care">Health Care</option>
+          <option value="utilities">Utilities</option>
+        </select>
+      </section>
+    );
+  }
   return (
     <>
-      <SearchBar onChange={handleSearch} value={search} />
+      <div>
+        <SearchBar onChange={handleSearch} value={search} />
+        <Filter />
+      </div>
       <Stock stocks={filterBySearch(stocks)} />
     </>
   );
