e637bc959863d911fefb03295f132138a3815e5e
AoWangPhilly <aowangphilly@gmail.com>
2021-08-12 16:48:17 -0400

    Add in button groups to select range of dates

--- src/components/GraphPage/GraphPage.js
+++ src/components/GraphPage/GraphPage.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Graph from "../Graph/Graphs";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 export default function GraphPage() {
   return (
     <div>
-      <Card className="text-center">
-      <Card.Header>AAPL Stocks</Card.Header>
+      <h1>AAPL Stock</h1>
+      <Card>
+        <ButtonGroup size="lg">
+          <Button>1 day</Button>
+          <Button>5 day</Button>
+          <Button>1 month</Button>
+          <Button>6 months</Button>
+          <Button>YTD</Button>
+          <Button>1 year</Button>
+          <Button>5 years</Button>
+          <Button>Max</Button>
+        </ButtonGroup>
         <Card.Body>
           <Graph />
         </Card.Body>
