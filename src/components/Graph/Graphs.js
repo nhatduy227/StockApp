a92f5336f91809e0626194bb16e8bde50ddffa8d
AoWangPhilly <aowangphilly@gmail.com>
2021-08-12 21:09:10 -0400

    Add in filter

--- src/components/Graph/Graphs.js
+++ src/components/Graph/Graphs.js
@@ -21,7 +21,7 @@ function clean() {
   for (let day of data) {
     day.date = new Date(day.date);
   }
-  
+
   data.sort((day1, day2) => {
     if (day1.date < day2.date) return -1;
     if (day1.date > day2.date) return 1;
@@ -35,11 +35,41 @@ function clean() {
   return data;
 }
 
-function Graph({setSelectedMode}) {
-  console.log(setSelectedMode);
+function getRangeOfStock(range) {
+  let cleanedData = clean();
+  let newData = []
+  let now = new Date();
+  switch (range) {
+    case "1d":
+      break;
+    case "5d":
+      break;
+    case "1m":
+      break;
+    case "6m":
+      break;
+    case "ytd":
+      break;
+    case "1y":
+      let yearAgo = new Date(new Date().setFullYear(new Date().getFullYear() - 1))
+      for (let day of cleanedData) {
+        let date = new Date(day.date);
+        if (date > yearAgo) {
+          newData.push(day);
+        }
+      }
+      return newData;
+    case "5y":
+      break;
+    case "max":
+      return cleanedData;
+  }
+}
+
+function Graph({ selectedRange }) {
   return (
     <ResponsiveContainer width="100%" height={400}>
-      <AreaChart data={clean()}>
+      <AreaChart data={getRangeOfStock(selectedRange)}>
         <defs>
           <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
             <stop offset="0%" stopColor="#85F485" stopOpacity={0.4} />
