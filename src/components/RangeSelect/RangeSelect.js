ba22ed06f1e9439996a258d2ea1f121ceff9ae19
AoWangPhilly <aowangphilly@gmail.com>
2021-08-21 22:52:35 -0400

    Rename variable

--- src/components/RangeSelect/RangeSelect.js
+++ src/components/RangeSelect/RangeSelect.js
@@ -2,32 +2,32 @@ import React from "react";
 
 import { Button,ButtonGroup} from "react-bootstrap";
 
-export default function RangeSelect({ setSelectedRange }) {
-  const setRange = (range) => {
-    setSelectedRange(range);
+export default function RangeSelect({ setRange }) {
+  const setNewRange = (range) => {
+    setRange(range);
   };
 
   return (
     <ButtonGroup size="lg">
-      <Button variant="light" onClick={() => setRange("5d")}>
+      <Button variant="light" onClick={() => setNewRange("5d")}>
         5 day
       </Button>
-      <Button variant="light" onClick={() => setRange("1m")}>
+      <Button variant="light" onClick={() => setNewRange("1m")}>
         1 month
       </Button>
-      <Button variant="light" onClick={() => setRange("6m")}>
+      <Button variant="light" onClick={() => setNewRange("6m")}>
         6 months
       </Button>
-      <Button variant="light" onClick={() => setRange("ytd")}>
+      <Button variant="light" onClick={() => setNewRange("ytd")}>
         YTD
       </Button>
-      <Button variant="light" onClick={() => setRange("1y")}>
+      <Button variant="light" onClick={() => setNewRange("1y")}>
         1 year
       </Button>
-      <Button variant="light" onClick={() => setRange("5y")}>
+      <Button variant="light" onClick={() => setNewRange("5y")}>
         5 years
       </Button>
-      <Button variant="light" onClick={() => setRange("max")}>
+      <Button variant="light" onClick={() => setNewRange("max")}>
         Max
       </Button>
     </ButtonGroup>
