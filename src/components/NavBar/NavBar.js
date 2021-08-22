5e1bd4d6c7acaba02fea8755dbba60975bbc8349
janicethitran <lfthuyvo@gmail.com>
2021-08-23 02:12:12 +0500

    Navbar was adjusted

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,32 +9,27 @@ function Navbar() {
   const closeMobileMenu = () => setClick(false);
 
   return (
-      <div className="navbar-container">
-        
-        <img src={process.env.PUBLIC_URL + '/favicon.ico'} alt="favicon"/>
-        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-          <h1 class="display-6">InvestiGators</h1>
-        </Link>
-        <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
-        </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
-            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/stockpage"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              StockPage
-            </Link>
-          </li>
-        </ul>
+    <div className="navbar-container">
+      <img src={process.env.PUBLIC_URL + "/favicon.ico"} alt="favicon" />
+      <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+        <h1 class="display-6">InvestiGators</h1>
+      </Link>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={click ? "fas fa-times" : "fas fa-bars"} />
       </div>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-item">
+          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/stockpage" className="nav-links" onClick={closeMobileMenu}>
+            StockPage
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
