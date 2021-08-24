1bcc2dfdd91b720a98e7b686bb75640ed3186850
Joey <huangjoey41@yahoo.com>
2021-08-24 15:15:45 -0400

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,41 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./NavBar.css";
+import { Container, Navbar, Nav, NavDropdown} from 'react-bootstrap';
+//import "./NavBar.css";
 
-function Navbar() {
-  const [click, setClick] = useState(false);
-
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
+function Navibar() {
   return (
-      <div className="navbar-container">
-        
-        <img src={process.env.PUBLIC_URL + '/favicon.ico'} alt="favicon"/>
-        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-          <h1 class="display-6">InvestiGators</h1>
-        </Link>
-        <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
-        </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
-            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/stockpage"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              StockPage
-            </Link>
-          </li>
-        </ul>
-      </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt="favicon"
+            src={process.env.PUBLIC_URL + '/favicon.ico'}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          InvestiGators</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+      
   );
 }
 
-export default Navbar;
+export default Navibar;
