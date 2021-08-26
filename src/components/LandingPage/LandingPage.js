aa6c26a7a44f43b5e7e33cb789a4a10f1877a2d1
Joey <huangjoey41@yahoo.com>
2021-08-26 15:01:21 -0400

    Removed unnecessary CSS files

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Image, Jumbotron, Button, } from 'react-bootstrap';
+import { Container, Row, Col, Image, Jumbotron} from 'react-bootstrap';
 import "./LandingPage.css";
 
 export default function LandingPage() {
