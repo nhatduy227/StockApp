36c1bb33cca2ac573348abc3859a7f03f668a65a
janicethitran <lfthuyvo@gmail.com>
2021-08-30 01:32:07 +0500

    carousel was added in a new page.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import React from "react";
 import StockPage from "./components/StockPage/StockPage";
+
+import BrowseStockPage from "./components/BrowseStockPage/BrowseStockPage";
+
 import LandingPage from "./components/LandingPage/LandingPage";
 import GraphPage from "./components/GraphPage/GraphPage";
 import Navbar from "./components/NavBar/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-      <Router>
-        <div className="grid-container">
-          <header>
-            {" "}
-            <Navbar />
-          </header>
-          <main>
-            <div className="content">
-              <div className="main">
-                <Switch>
-                  <Route path="/" exact component={LandingPage} />
-                  <Route path="/stockpage" exact component={StockPage} />
-                  <Route path="/graphpage/:ticker" component={GraphPage} />
-                </Switch>
-              </div>
+    <Router>
+      <div className="grid-container">
+        <header>
+          {" "}
+          <Navbar />
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Switch>
+                <Route path="/" exact component={LandingPage} />
+                <Route path="/stockpage" exact component={StockPage} />
+
+                <Route
+                  path="/browsestockpage"
+                  exact
+                  component={BrowseStockPage}
+                />
+
+                <Route path="/graphpage/:ticker" component={GraphPage} />
+              </Switch>
             </div>
-          </main>
-        </div>
-      </Router>
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/BrowseStockPage/BrowseStockPage.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from "react";
+import Fade from "react-reveal/Fade";
+import SearchBar from "../SearchBar/SearchBar";
+import { Link } from "react-router-dom";
+import "./BrowseStockPage.css";
+import db from "../../firebase";
+
+export default function BrowseStockPage() {
+  const [stocks, setStocks] = useState([
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+      score: 76,
+      sector: "Consumer Discretionary",
+      title: "Amazon",
+      symbol: "AMZN",
+    },
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+      score: 76,
+      sector: "Consumer Discretionary",
+      title: "Amazon",
+      symbol: "AMZN",
+    },
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+      score: 76,
+      sector: "Consumer Discretionary",
+      title: "Amazon",
+      symbol: "AMZN",
+    },
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+      score: 76,
+      sector: "Consumer Discretionary",
+      title: "Amazon",
+      symbol: "AMZN",
+    },
+  ]);
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("all");
+  useEffect(() => {
+    db.collection("stocks").onSnapshot((snapshot) => {
+      setStocks(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          image: doc.data().image,
+          score: doc.data().score,
+          title: doc.data().title,
+          sector: doc.data().sector,
+          symbol: doc.data().symbol,
+        }))
+      );
+    });
+  }, []);
+  function handleSearch(e) {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+  }
+
+  function filterBySearch(array) {
+    const filtered_by_search = array.filter((s) =>
+      s.title.toLowerCase().includes(search.toLowerCase())
+    );
+    return filter === "all"
+      ? filtered_by_search
+      : filtered_by_search.filter((s) => s.sector.toLowerCase() === filter);
+  }
+  function compareScore(a, b) {
+    if (a.score > b.score) return -1;
+    if (a.score < b.score) return 1;
+    return 0;
+  }
+
+  function TopStock({ stocks }) {
+    const topstocks = stocks.filter((s) => s.score > 70).sort(compareScore);
+    return <Stock stocks={topstocks} />;
+  }
+  function Stock({ stocks }) {
+    if (stocks.length === 0) {
+      return <h2>No Matching Stock</h2>;
+    }
+    return (
+      <div>
+        <Fade bottom cascade={true}>
+          <ul className="stocks">
+            {stocks.map((stock) => (
+              <li key={stock.id}>
+                <div className="stock">
+                  <img src={stock.image} alt={stock.title}></img>
+                  <p>
+                    {stock.title} - {stock.sector}
+                  </p>
+                  <div className="stock-price">
+                    <div className="stock-score">{stock.score}</div>
+                    <Link
+                      to={{
+                        pathname: `/graphpage/${stock.symbol}`,
+                      }}
+                      className="nav-links"
+                    >
+                      <button className="button primary">View Stock</button>
+                    </Link>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Fade>
+      </div>
+    );
+  }
+  function Filter() {
+    return (
+      <section id="Filters">
+        <select
+          className="filter"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="all">All</option>
+          <option value="energy">Energy</option>
+          <option value="financials">Financials</option>
+          <option value="industrials">Industrials</option>
+          <option value="real estate">Real Estate</option>
+          <option value="consumer staples">Consumer Staples</option>
+          <option value="consumer discretionary">Consumer Discretionary</option>
+          <option value="information technology">Information Technology</option>
+          <option value="health care">Health Care</option>
+          <option value="utilities">Utilities</option>
+        </select>
+      </section>
+    );
+  }
+  console.log(stocks);
+  return (
+    <div>
+      <div className="filter-div">
+        <SearchBar onChange={handleSearch} value={search} />
+        {search === "" ? null : <Filter />}
+      </div>
+      {search === "" ? null : <Stock stocks={filterBySearch(stocks)} />}
+      <div className="trending-div">
+        <h1>Top Trending</h1>
+        <TopStock stocks={stocks} />
+      </div>
+    </div>
+  );
+}
